9516ea48d3ccdfe61dd831dd96930e1623efcfb4
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2019-04-16 17:49:56 -0400

    Toolkit established
    Added build tools:
    
    * webpack (server and client)
    * eslint
    * prettier
    * babel
    * graphql-hooks
    * styled-components
    * external graphql files
    * react-helmet
    * prop-types

---
+++ webpack.base.config.js
@@ -0,0 +1,91 @@
+/*eslint-env node*/
+const glob = require("glob");
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  output: {
+    publicPath: "/",
+    filename: "[name].js"
+  },
+  devtool: "inline-source-map",
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          emitWarning: true,
+          failOnError: false,
+          failOnWarning: false
+        }
+      },
+      {
+        // Transpiles ES6-8 into ES5
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        // Loads the javacript into html template provided.
+        // Entry point is set below in HtmlWebPackPlugin in Plugins
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        // Loads images into CSS and Javascript files
+        test: /\.jpg$/,
+        use: [{ loader: "url-loader" }]
+      },
+      {
+        // Loads CSS into a file when you import it via Javascript
+        // Rules are set in MiniCssExtractPlugin
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.graphql$/,
+        use: ["raw-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: "./index.html",
+      excludeChunks: ["server"]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new PurgecssPlugin({
+      paths: () =>
+        glob.sync(`${path.join(__dirname, "components")}/**/*`, {
+          nodir: true
+        })
+    })
+  ]
+};
---
+++ webpack.server.config.js
@@ -0,0 +1,18 @@
+/*eslint-env node*/
+const nodeExternals = require("webpack-node-externals");
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.config");
+require("babel-polyfill");
+
+module.exports = merge(baseConfig, {
+  entry: {
+    server: ["babel-polyfill", "./server.js"]
+  },
+  target: "node",
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false // and __filename return blank or /
+  },
+  externals: [nodeExternals()]
+});
