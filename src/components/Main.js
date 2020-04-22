4cdb4d247195fc5d6ae318f62ed43a01054f276d
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2020-04-22 16:07:01 -0400

    Add husky pre-commit hook for Prettier
    * update deps

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import { css } from "styled-components/macro";
 import tw from "twin.macro";
 import Avatar from "./Avatar";
