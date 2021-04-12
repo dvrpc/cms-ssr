4a25eb1805a277170bf6d37fc1994b1adddf1a98
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2021-04-12 15:40:46 -0400

    reduce footer image sizes

--- src/configs/ads.js
+++ src/configs/ads.js
@@ -3,14 +3,14 @@ export default [
     href: "https://www.airqualitypartnership.org/",
     src: "https://www.dvrpc.org/asp/ads/aqp/default.ashx",
     label: "Air Quality Partnership",
-    style: "width: 190px;",
+    style: "width: 176px;",
     new_window: true,
   },
   {
     href: "/COVID19Resources/",
     src: "https://www.dvrpc.org/img/5545_CovidNoDVRPCLogo-01.png",
     label: "COVID-19 Resources",
-    style: "width: 210px;",
+    style: "width: 176px;",
   },
   {
     href:
@@ -24,13 +24,13 @@ export default [
     src: "https://www.dvrpc.org/img/gatsby/5546_Dispatches.png",
     href: "https://indd.adobe.com/view/39a8d5db-23df-4324-9099-4562dd8ed59b",
     label: "Dispatches from Alternate Futures",
-    style: "width: 200px;",
+    style: "width: 176px;",
     new_window: true,
   },
   {
     href: "/News/",
     src: "https://www.dvrpc.org/img/gatsby/5546_DVRPCnews.png",
     label: "DVRPC News",
-    style: "width: 200px;",
+    style: "width: 176px;",
   },
 ];
