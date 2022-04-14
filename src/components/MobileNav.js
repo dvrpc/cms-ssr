5ea673cfdb043da8605bdf1b95f2feb364e1b917
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2022-04-14 12:18:38 -0400

    remove template and extra nav render

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -37,81 +37,8 @@ const MobileNav = ({ additionalLinks }) => {
             </div>
           );
         })}
-        {rootNavArr.map((item, index) => {
-          return (
-            <div key={index} className="pl-4 py-4 flex-auto text-3xl">
-              <Link
-                className="no-underline hover:underline my-12"
-                to={item.href}
-                dangerouslySetInnerHTML={{ __html: item.link }}
-              />
-            </div>
-          );
-        })}
       </div>
     </div>
-
-    // <div class="xnav">
-    //   <div class="xnav-wrapper">
-    //     <ul class="sf-menu">
-    //       <li>
-    //         <a href="#">Home</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Cupcakes</a>
-    //         <ul>
-    //           <li>
-    //             <a href="#">1.1 Cupcakes</a>
-    //           </li>
-    //           <li>
-    //             <a href="#">1.2 Cupcakes</a>
-    //           </li>
-    //           <li>
-    //             <a href="#">1.3 Cupcakes</a>
-    //             <ul>
-    //               <li>
-    //                 <a href="#">2.1 Cupcakes</a>
-    //               </li>
-    //               <li>
-    //                 <a href="#">2.2 Cupcakes</a>
-    //               </li>
-    //               <li>
-    //                 <a href="#">2.3 Cupcakes</a>
-    //               </li>
-    //             </ul>
-    //           </li>
-    //         </ul>
-    //       </li>
-    //       <li>
-    //         <a href="#">Doughnuts</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Icecream</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Banana Split</a>{" "}
-    //       </li>
-    //       <li>
-    //         <a href="#">Brownies</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Lollipop</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Pixie Stix</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Bubble Gum</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Chocolate Chips</a>
-    //       </li>
-    //       <li>
-    //         <a href="#">Cherries</a>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </div>
   );
 };
 export default MobileNav;
