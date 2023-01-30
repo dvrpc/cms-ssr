673d766fc4f8ea05877643a243a1406aff83ef8b
Tyler <hachadorian98@gmail.com>
2023-01-30 14:44:31 -0500

    relates to #19: styling changes to app cards on home

--- src/components/datacenter/AppCard.js
+++ src/components/datacenter/AppCard.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React from 'react';
 
 const AppCard = ({ node }) => {
   const { field_product_id: id, body, title, field_url: url } = node.entity[0];
   const len = 1000;
   const text = body
-    ? body.processed.slice(0, len) + body.processed.slice(len).split(" ")[0]
-    : "";
+    ? body.processed.slice(0, len) + body.processed.slice(len).split(' ')[0]
+    : '';
   return (
     <div
-      className="flex md:min-w-[375px] break-inside-avoid flex-col bg-white p-[0.25em] text-lg"
+      className="flex break-inside-avoid flex-col bg-white p-[0.25em] text-lg md:min-w-[375px]"
       key={id}
     >
-      <a href={url.uri} className="h-[40%]">
+      <a href={url.uri}>
         <img
-          className="h-full w-full object-cover "
+          className="h-[150px] w-full object-cover object-center"
           src={`https://www.dvrpc.org/asp/pubs/402px/${id}.png`}
         />
       </a>
-      <div className="divide-y p-4 pb-0">
-        <h3 className="mt-0 w-full text-[#0078ae]">
+      <div className="divide-y divide-[#53a3c7]">
+        <h4 className="m-0 mb-[1rem] w-full px-2 text-[#0078ae]">
           <a className="no-underline hover:underline" href={url.uri}>
             {title}
           </a>
-        </h3>
-        {text && <div
-          className="text-slate-400"
-          dangerouslySetInnerHTML={{
-            __html: text === body.processed ? text : `${text}&hellip;`,
-          }}
-        ></div>}
+        </h4>
+        {text && (
+          <div
+            className="px-2 text-base text-slate-400"
+            dangerouslySetInnerHTML={{
+              __html:
+                text === body.processed
+                  ? text.slice(0, 150) + '...'
+                  : `${text}&hellip;`,
+            }}
+          ></div>
+        )}
       </div>
     </div>
   );
