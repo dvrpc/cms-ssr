c7f0741b809fa37535b75de0018fb86034e189f2
Michael Ruane <mruane@dvrpc.org>
2023-02-21 09:46:00 -0500

    relates to #19: banner responsiveness

--- src/components/datacenter/Banner.js
+++ src/components/datacenter/Banner.js
@@ -4,7 +4,7 @@ import DVRPCbg from "../../images/dvrpc-transparent.png";
 const Banner = () => {
   return (
     <div
-      className="relative flex h-[var(--height-banner)] w-full overflow-hidden after:absolute
+      className="relative flex h-[calc(var(--height-banner)*2.5)] md:h-[calc(var(--height-banner)*1.6)] lg:h-[calc(var(--height-banner)*1.2)] xl:h-[var(--height-banner)] w-full overflow-hidden after:absolute
           after:bottom-4 after:right-0 after:block after:bg-gradient-to-r 
           after:from-transparent after:via-white/80 after:to-white/80 after:p-1 
           after:px-2 after:pl-64 after:text-sm after:text-gray-900 after:content-[var(--content-photo-credits)]"
@@ -17,7 +17,7 @@ const Banner = () => {
         <form
           action="https://catalog.dvrpc.org/dataset/"
           method="GET"
-          className="my-auto w-1/3"
+          className="my-auto w-full md:w-2/3 lg:w-1/2 xl:w-1/3"
         >
           <h1 className="text-white">DVRPC Data Center</h1>
           <p>
@@ -29,7 +29,7 @@ const Banner = () => {
             name="q"
             placeholder="Search datasets..."
             autoFocus
-            className="mt-8 hidden w-full rounded-lg bg-[#c6dae9] text-xl leading-none outline-none placeholder:italic placeholder:text-slate-400 placeholder:text-left placeholder:text-lg placeholder:tracking-wider  md:block"
+            className="mt-8 w-full rounded-lg bg-[#c6dae9] text-xl leading-none outline-none placeholder:italic placeholder:text-slate-400 placeholder:text-left placeholder:text-lg placeholder:tracking-wider  block"
           />
         </form>
       </div>
