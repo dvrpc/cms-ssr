4871ad35f5ab7b7dd17915af7ef1392e30776220
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2023-03-02 22:45:17 -0500

    Remove unneeded postcss deps

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   plugins: [
-    require("postcss-at-rules-variables"),
-    require("postcss-each"),
-    require("postcss-import"),
-    require("postcss-for"),
-    require("postcss-custom-properties"),
-    require("postcss-calc")({ selectors: true }),
     require("tailwindcss"),
     require("postcss-nested"),
     require("postcss-preset-env")({ stage: 1 }),
