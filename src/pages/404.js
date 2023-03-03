5a8446e4357dd8572c2ad4539fb0780cf77d16f2
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2023-03-03 01:24:59 -0500

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const NotFoundPage = () => (
+  <article>
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </article>
+);
+
+export default NotFoundPage;
