7a09d409081c5e3e5fbd52e7edcf6be838cf3299
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2023-03-06 00:21:48 -0500

    Add mobile nav menu

--- src/components/LogoBar.js
+++ src/components/LogoBar.js
@@ -4,7 +4,7 @@ import SocialMedia from "./SocialMedia";
 import TopNav from "./TopNav";
 
 const LogoBar = () => (
-  <div className="container mx-auto grid px-8 md:grid-cols-[auto_1fr]">
+  <div className="container mx-auto grid grid-cols-2 items-center px-8 md:grid-cols-[auto_1fr]">
     <a href="/" className="row-span-2">
       <Icon use={Dvrpc} scale={null} className="mx-auto my-8 h-[70.5px]" />
     </a>
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -43,9 +43,9 @@ const rootNavArr = [
 const RootNav = ({ data }) => {
   return data.map((item, index) => {
     return (
-      <div key={index} className="shrink-0 text-center md:leading-none">
+      <div key={index} className="md:leading-none">
         <Link
-          className="pl-2 font-bold no-underline hover:underline"
+          className="font-bold no-underline hover:underline"
           to={item.href}
           dangerouslySetInnerHTML={{ __html: item.link }}
         />
@@ -56,10 +56,21 @@ const RootNav = ({ data }) => {
 
 const TopNav = ({ menu }) => {
   return (
-    <div className="mb-7 self-end overflow-x-auto overflow-y-hidden md:ml-4 lg:ml-auto">
-      <nav className="ml-auto hidden gap-3 divide-x divide-[#0078ae] font-medium text-[color:var(--color-default)] md:flex">
+    <div className="row-span-2 md:ml-8">
+      <nav className="ml-auto hidden grid-cols-3 grid-rows-2 justify-end gap-y-1 gap-x-4 text-[color:var(--color-default)] md:grid lg:gap-y-3 xl:flex xl:grid-cols-9">
         <RootNav data={rootNavArr} />
       </nav>
+      <div className="p-8 text-right text-[color:var(--color-default)] md:hidden">
+        <details className="group">
+          <summary className="cursor-pointer list-none text-3xl font-bold leading-none [&::marker]:hidden">
+            <span className="block group-open:hidden">☰</span>
+            <span className="hidden group-open:block">✖</span>
+          </summary>
+          <div className="absolute top-[calc(4rem+70.5px)] right-0 left-0 z-30 h-0 overflow-auto bg-gray-50 p-8 pt-0 text-left text-xl font-normal leading-10 transition-[height] duration-300 ease-out group-open:h-[calc(100vh-4rem-70.5px)]">
+            <RootNav data={rootNavArr} />
+          </div>
+        </details>
+      </div>
     </div>
   );
 };
