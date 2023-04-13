8859a3c42d9452cfceeb39e018e376a842bc4e38
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-04-13 09:26:55 -0400

    wrap in body component and add staff contact
    staff contact component

--- src/pages/boardlist.js
+++ src/pages/boardlist.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Body from "../components/Body";
+import StaffContact from "../components/StaffContact";
 
 const BoardDetails = ({ committee }) => {
   return (
@@ -41,36 +43,41 @@ const BoardListPage = ({ serverData }) => {
   const { Board, Officers } = serverData;
 
   return (
-    <div>
-      <h1>DVRPC Board</h1>
-      <p>
-        The Board is an 18-member body having the authority and responsibility
-        to make decisions affecting the entire organization as well as the
-        nine-county region. It creates and defines the duties of the Office of
-        the Executive Director and the various DVRPC committees; and approves
-        and adopts the annual planning work program. In its capacity as the
-        Metropolitan Planning Organization (MPO) for the region, the Board
-        establishes regional transportation policies, determines allocation of
-        available transportation funds and prioritizes transportation projects
-        for the region. In addition to transportation planning for highways,
-        transit, airports and freight, the agency develops plans and policies
-        for other regional physical planning elements such as land use, air
-        quality, housing, water supply and water quality.{" "}
-      </p>
-      <h2>Officers:</h2>
-      {Officers.map((officer) => {
-        return (
-          <>
-            <strong>{officer.Title}:</strong> <span>{officer.Name}</span>
-            <br />
-          </>
-        );
-      })}
-      <h2>Board Members and Alternates</h2>
-      {Board.map((committee) => (
-        <BoardDetails committee={committee} />
-      ))}
-    </div>
+    <>
+      <Body title="DVRPC Board">
+        <div>
+          <p>
+            The Board is an 18-member body having the authority and
+            responsibility to make decisions affecting the entire organization
+            as well as the nine-county region. It creates and defines the duties
+            of the Office of the Executive Director and the various DVRPC
+            committees; and approves and adopts the annual planning work
+            program. In its capacity as the Metropolitan Planning Organization
+            (MPO) for the region, the Board establishes regional transportation
+            policies, determines allocation of available transportation funds
+            and prioritizes transportation projects for the region. In addition
+            to transportation planning for highways, transit, airports and
+            freight, the agency develops plans and policies for other regional
+            physical planning elements such as land use, air quality, housing,
+            water supply and water quality.{" "}
+          </p>
+          <h2>Officers:</h2>
+          {Officers.map((officer) => {
+            return (
+              <>
+                <strong>{officer.Title}:</strong> <span>{officer.Name}</span>
+                <br />
+              </>
+            );
+          })}
+          <h2>Board Members and Alternates</h2>
+          {Board.map((committee) => (
+            <BoardDetails committee={committee} />
+          ))}
+        </div>
+      </Body>
+      <StaffContact />
+    </>
   );
 };
 
