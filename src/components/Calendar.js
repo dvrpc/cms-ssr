5566f541b87b8b96c831dd87d5444a9db4134a53
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-04-20 11:31:18 -0400

    refactor calendar datetime creation and more generic for partner events page

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Calendar = ({ data }) => {
+const Calendar = ({ data, header }) => {
   const renderedDate = new Date(data[0].StartDate);
   const previousMonth = new Date(
     renderedDate.getFullYear(),
@@ -17,23 +17,40 @@ const Calendar = ({ data }) => {
     <>
       <div className="card">
         <h2>
-          {renderedDate.toLocaleDateString("default", {
-            month: "long",
-            year: "numeric",
-          })}
+          {!header
+            ? renderedDate.toLocaleDateString("default", {
+                month: "long",
+                year: "numeric",
+              })
+            : header}
         </h2>
         <div className="divide-y">
           {data.map((event) => {
-            const eventDate = new Date(event.StartDate + " " + event.StartTime);
+            const eventDate = new Date(event.StartDate);
+            if (event.StartTime) {
+              const hoursMinutes = event.StartTime.split(":");
+              eventDate.setHours(hoursMinutes[0], hoursMinutes[1]);
+            }
             return (
               <div className="flex items-center space-x-4">
                 <p className="text-center">
-                  <strong className="text-xl">{eventDate.getDate()}</strong>
+                  <strong className="text-xl">
+                    {!header
+                      ? eventDate.getDate()
+                      : eventDate
+                          .toLocaleDateString("default", {
+                            month: "short",
+                            day: "numeric",
+                          })
+                          .toUpperCase()}
+                  </strong>
                   <br />
-                  <span>
-                    {eventDate.getHours()}:
-                    {String(eventDate.getMinutes()).padStart(2, "0")}
-                  </span>
+                  {event.StartTime && (
+                    <span>
+                      {eventDate.getHours() % 12}:
+                      {String(eventDate.getMinutes()).padStart(2, "0")}
+                    </span>
+                  )}
                 </p>
                 <p>
                   <a className="underline" href={event.Info}>
@@ -52,23 +69,25 @@ const Calendar = ({ data }) => {
             );
           })}
         </div>
-        <div className="flex w-full">
-          <a
-            href={`/calendar/${previousMonth.getFullYear()}/${
-              previousMonth.getMonth() + 1
-            }`}
-          >
-            ⟵ {previousMonth.toLocaleString("default", { month: "long" })}
-          </a>
-          <a
-            className="ml-auto"
-            href={`/calendar/${nextMonth.getFullYear()}/${
-              nextMonth.getMonth() + 1
-            }`}
-          >
-            {nextMonth.toLocaleString("default", { month: "long" })} ⟶
-          </a>
-        </div>
+        {!header && (
+          <div className="flex w-full">
+            <a
+              href={`/calendar/${previousMonth.getFullYear()}/${
+                previousMonth.getMonth() + 1
+              }`}
+            >
+              ⟵ {previousMonth.toLocaleString("default", { month: "long" })}
+            </a>
+            <a
+              className="ml-auto"
+              href={`/calendar/${nextMonth.getFullYear()}/${
+                nextMonth.getMonth() + 1
+              }`}
+            >
+              {nextMonth.toLocaleString("default", { month: "long" })} ⟶
+            </a>
+          </div>
+        )}
       </div>
     </>
   );
