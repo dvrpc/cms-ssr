afad32a7631df2846a050b936ba1612766e62dab
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-04-20 10:58:55 -0400

    styling updates to Calendar component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Body from "./Body";
 
 const Calendar = ({ data }) => {
   const renderedDate = new Date(data[0].StartDate);
@@ -15,61 +14,63 @@ const Calendar = ({ data }) => {
   );
 
   return (
-    <Body title="Calendar">
-      <div>
-        The DVRPC Meeting Calendar is updated on a frequent basis with all
-        internal and external DVRPC sponsored events. DVRPC does not permit
-        external videotaping, audiotaping, or recording at its committee
-        meetings. External taping is permitted at DVRPC's Board meeting when it
-        is held in person; please inform staff of your intention to do so.
-        Please note, since DVRPC is holding its committee meetings, including
-        Board meetings, remotely via Zoom, staff may record the meeting to take
-        meeting minutes, to share meeting content with committee members or
-        members of the public, for educational purposes, or for other reasons
-        not listed here. By continuing to stay on the remote meeting or entering
-        a meeting already in progress, the attendee consents to be recorded.
-      </div>
-      <div>
+    <>
+      <div className="card">
         <h2>
           {renderedDate.toLocaleDateString("default", {
             month: "long",
             year: "numeric",
           })}
         </h2>
-        {data.map((event) => (
-          <>
-            <p>
-              <a className="underline" href={event.Info}>
-                {event.Title}
-              </a>
-              {event.Location && (
-                <>
+        <div className="divide-y">
+          {data.map((event) => {
+            const eventDate = new Date(event.StartDate + " " + event.StartTime);
+            return (
+              <div className="flex items-center space-x-4">
+                <p className="text-center">
+                  <strong className="text-xl">{eventDate.getDate()}</strong>
                   <br />
-                  <span className="italic text-gray-400">{event.Location}</span>
-                </>
-              )}
-            </p>
-          </>
-        ))}
+                  <span>
+                    {eventDate.getHours()}:
+                    {String(eventDate.getMinutes()).padStart(2, "0")}
+                  </span>
+                </p>
+                <p>
+                  <a className="underline" href={event.Info}>
+                    {event.Title}
+                  </a>
+                  {event.Location && (
+                    <>
+                      <br />
+                      <span className="italic text-gray-400">
+                        {event.Location}
+                      </span>
+                    </>
+                  )}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex w-full">
+          <a
+            href={`/calendar/${previousMonth.getFullYear()}/${
+              previousMonth.getMonth() + 1
+            }`}
+          >
+            ⟵ {previousMonth.toLocaleString("default", { month: "long" })}
+          </a>
+          <a
+            className="ml-auto"
+            href={`/calendar/${nextMonth.getFullYear()}/${
+              nextMonth.getMonth() + 1
+            }`}
+          >
+            {nextMonth.toLocaleString("default", { month: "long" })} ⟶
+          </a>
+        </div>
       </div>
-      <div className="flex w-full">
-        <a
-          href={`/calendar/${previousMonth.getFullYear()}/${
-            previousMonth.getMonth() + 1
-          }`}
-        >
-          ⟵ {previousMonth.toLocaleString("default", { month: "long" })}
-        </a>
-        <a
-          className="ml-auto"
-          href={`/calendar/${nextMonth.getFullYear()}/${
-            nextMonth.getMonth() + 1
-          }`}
-        >
-          {nextMonth.toLocaleString("default", { month: "long" })} ⟶
-        </a>
-      </div>
-    </Body>
+    </>
   );
 };
 
