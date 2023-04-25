1235cd58b28dbc670087d19e8be2430993e0ab60
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-04-25 09:28:33 -0400

    styling updates

--- src/pages/products/recent.js
+++ src/pages/products/recent.js
@@ -1,40 +1,46 @@
 import React from "react";
+import Body from "../../components/Body";
+import StaffContact from "../../components/StaffContact";
 
 const RecentProductsPage = ({ serverData }) => {
   return (
-    <div>
-      <h1>Recent Products</h1>
-      <p>
-        Here is a list of products released by DVRPC in the past year, sorted
-        newest-to-oldest, by date the product was added to our website.
-      </p>
-      <hr />
-      <table className="table-auto">
-        <thead>
-          <tr className="font-bold">
-            <td>Product</td>
-            <td>Title</td>
-            <td>Date</td>
-          </tr>
-        </thead>
-        <tbody>
-          {serverData.map((product) => (
-            <tr>
-              <td>{product.Id}</td>
-              <td>
-                <a
-                  href={`https://www.dvrpc.org/products/${product.Id}`}
-                  className="underline"
-                >
-                  {product.Title}
-                </a>
-              </td>
-              <td>{new Date(product.DateLive).toLocaleDateString()}</td>
+    <>
+      <Body title="Recent Products">
+        <div className="mt-4">
+          Here is a list of products released by DVRPC in the past year, sorted
+          newest-to-oldest, by date the product was added to our website.
+        </div>
+        <br />
+        <table className="w-3/4 table-auto">
+          <thead>
+            <tr className="font-bold">
+              <td>Product</td>
+              <td>Title</td>
+              <td>Date</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {serverData
+              .sort((a, b) => new Date(b.DateLive) - new Date(a.DateLive))
+              .map((product) => (
+                <tr>
+                  <td>{product.Id}</td>
+                  <td>
+                    <a
+                      href={`https://www.dvrpc.org/products/${product.Id}`}
+                      className="underline"
+                    >
+                      {product.Title}
+                    </a>
+                  </td>
+                  <td>{new Date(product.DateLive).toLocaleDateString()}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </Body>
+      <StaffContact />
+    </>
   );
 };
 
@@ -42,7 +48,9 @@ export default RecentProductsPage;
 
 export async function getServerData() {
   try {
-    const res = await fetch("https://www.dvrpc.org/api/products");
+    const res = await fetch(
+      "https://www.dvrpc.org/api/products?onlyFeatured=false"
+    );
     if (!res.ok) {
       throw new Error("Response failed");
     }
--- src/pages/products/search.js
+++ src/pages/products/search.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { navigate } from "gatsby";
+import Body from "../../components/Body";
+import StaffContact from "../../components/StaffContact";
 
 const SearchPage = () => {
   const params = new URLSearchParams(location.search);
@@ -11,7 +13,11 @@ const SearchPage = () => {
 
   useEffect(() => {
     (async () => {
-      if (!query) return;
+      if (!query) {
+        setProducts([]);
+        setResults([]);
+        return;
+      }
       const [products, results] = await Promise.all([
         fetch(`https://www.dvrpc.org/api/Products?keywords=${query}`).then(
           (res) => res.json()
@@ -26,80 +32,95 @@ const SearchPage = () => {
   }, [query]);
 
   return (
-    <div className="p-4">
-      <div className="mb-2 overflow-hidden rounded-sm bg-white">
-        <h1>Products Search</h1>
-        <p>
-          The DVRPC Products Database is continuously updated as reports and
-          other items become available. Visitors can view publication abstracts
-          and/or full PDF versions for over 1,000 reports from the late 1980s to
-          present. You can also see a <a href="/">quick list</a> of reports
-          published in the last year.
-        </p>
-      </div>
-      <hr />
-      <form
-        className="mb-2 overflow-hidden rounded-sm bg-white"
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (!querySet.has(keywordRef.current.value))
-            querySet.add(keywordRef.current.value);
-          keywordRef.current.value = "";
-          navigate(`?q=${Array.from(querySet).join(",")}`);
-        }}
-      >
-        <h1>Keyword Search</h1>
-        <input
-          placeholder="Search by keyword"
-          ref={keywordRef}
-          className="focus:shadow-outline my-2 appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
-        ></input>
-        <div>
-          {query.length > 0 &&
-            query.split(",").map((param) => (
-              <button
-                className="mr-2 cursor-pointer rounded-full bg-[#999999] p-1 py-1 px-4 text-white"
-                onClick={() => {
-                  querySet.delete(param);
-                  setProducts([]);
-                  navigate(`?q=${Array.from(querySet).join(",")}`);
-                }}
-                type="button"
-              >
-                {param} &times;
-              </button>
-            ))}
+    <>
+      <Body title="Products Search">
+        <div className="mb-2 overflow-hidden rounded-sm">
+          <p>
+            The DVRPC Products Database is continuously updated as reports and
+            other items become available. Visitors can view publication
+            abstracts and/or full PDF versions for over 1,000 reports from the
+            late 1980s to present. You can also see a{" "}
+            <a href="/products/recent">quick list</a> of reports published in
+            the last year.
+          </p>
         </div>
-      </form>
-
-      <div className="mb-2 grid grid-cols-2 overflow-hidden rounded-sm bg-white">
-        <div>
-          <h1>DVRPC Products</h1>
-          {products.length > 0 &&
-            products.map((product) => (
-              <div className="flex max-h-min text-sm">
-                <img
-                  src={`https://www.dvrpc.org/asp/pubs/100px/${product.Id}.png`}
-                  className="h-1/2 max-w-fit object-cover"
-                ></img>
-                {product.Title}
+        <div className="card">
+          <h2 className="flex items-center">
+            <span>Results</span>
+            <span className="ml-auto text-base">
+              {!query ? "No" : products.length + results.length} result(s) found
+            </span>
+          </h2>
+          <form
+            className="mb-2 overflow-hidden rounded-sm"
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (!querySet.has(keywordRef.current.value))
+                querySet.add(keywordRef.current.value);
+              keywordRef.current.value = "";
+              navigate(`?q=${Array.from(querySet).join(",")}`);
+            }}
+          >
+            <input
+              placeholder="Search by keyword"
+              ref={keywordRef}
+              className="my-2 appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+            ></input>
+            <div>
+              {query.length > 0 &&
+                query.split(",").map((param) => (
+                  <button
+                    className="mr-2 cursor-pointer rounded-full bg-[#999999] p-1 py-1 px-4 text-white"
+                    onClick={() => {
+                      querySet.delete(param);
+                      navigate(`?q=${Array.from(querySet).join(",")}`);
+                    }}
+                    type="button"
+                  >
+                    {param} &times;
+                  </button>
+                ))}
+            </div>
+          </form>
+          {(products.length > 0 || results.length > 0) && (
+            <div className="mb-2 grid grid-cols-2">
+              <div>
+                <h3>DVRPC Products</h3>
+                {products.length > 0 &&
+                  products.map((product) => (
+                    <div className="flex text-sm">
+                      <img
+                        src={`https://www.dvrpc.org/asp/pubs/100px/${product.Id}.png`}
+                        className="object-cover"
+                      ></img>
+                      {product.Title}
+                    </div>
+                  ))}
               </div>
-            ))}
-        </div>
-        <div>
-          <h1>All Results</h1>
-          {results.items.length > 0 &&
-            results.map((result) => (
-              <div className="mb-4 space-y-1">
-                <a href={result.formattedUrl} className="text-lg font-bold">
-                  {result.title}
-                </a>
-                <div dangerouslySetInnerHTML={{ __html: result.htmlSnippet }} />
+              <div>
+                <h3>All Results</h3>
+                {results.length > 0 &&
+                  results.map((result) => (
+                    <div className="mb-4 space-y-1">
+                      <a
+                        href={result.formattedUrl}
+                        className="text-lg font-bold"
+                      >
+                        {result.title}
+                      </a>
+                      <div
+                        className="text-sm"
+                        dangerouslySetInnerHTML={{ __html: result.htmlSnippet }}
+                      />
+                    </div>
+                  ))}
               </div>
-            ))}
+            </div>
+          )}
         </div>
-      </div>
-    </div>
+      </Body>
+      <StaffContact />
+    </>
   );
 };
 
