7adde544622f94546e4598d484a26ffa2bddad33
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-11-30 13:02:48 -0500

    article page and images

--- src/pages/{NodeArticle.path__alias}.js
+++ src/pages/{NodeArticle.path__alias}.js
@@ -1,18 +1,176 @@
 import * as React from "react";
 import { graphql } from "gatsby";
-import DefaultPage from "../components/DefaultPage";
 import HeadTemplate, { themeToCustomVars } from "../components/HeadTemplate";
+import StaffContact from "../components/StaffContact";
+import HtmlParser from "../components/HtmlParser";
 
-const Page = ({ data: { navItem, nodePage, staffContact } }) => {
-  const { body, ...props } = nodePage;
+const Page = ({ data: { nodePage } }) => {
+  const { body, title, path, relationships } = nodePage;
+
   return (
-    <DefaultPage
-      {...props}
-      body={body.processed}
-      location={nodePage.path.alias}
-      navItem={navItem}
-      staffContact={staffContact}
-    />
+    <>
+      <div className="container mx-auto my-4 grid gap-x-12 print:block print:!max-w-full print:text-black sm:grid-cols-1 md:grid-cols-4">
+        <div className="px-4 pt-0 print:p-0 md:col-span-3 md:col-start-2 md:row-start-2 md:p-0">
+          <div className="flex w-full">
+            <h1 className="max-w-[80ch] px-4 text-4xl font-bold text-[color:var(--color-h1)] print:max-w-full print:p-0 md:col-span-1 md:col-start-2 md:p-0">
+              {title}
+            </h1>
+          </div>
+          <main className="mt-4">
+            <article className="border-b-2">
+              <div className="flex">
+                <p className="mt-auto mb-0">
+                  <p className="w-full text-sm text-[#595959]">
+                    {new Date(nodePage.created).toLocaleDateString("en-US", {
+                      month: "long",
+                      day: "numeric",
+                      year: "numeric",
+                    })}
+                  </p>
+                  {body.summary && (
+                    <blockquote className="flex-1 border-l-8 border-[#91BEDC] border-opacity-[.45] pl-2 italic md:mr-4">
+                      {body.summary}
+                    </blockquote>
+                  )}
+                </p>
+                <img
+                  className="border border-2 p-0.5"
+                  src={relationships.field_image.url}
+                />
+              </div>
+              <HtmlParser html={body.processed ?? ""} />
+              {nodePage.relationships && (
+                <p className="m-0 text-[#7A7A7A]">
+                  {relationships.field_tags.map((tag, idx) => (
+                    <span>
+                      {tag.name}
+                      {idx !== relationships.field_tags.length - 1 && ", "}
+                    </span>
+                  ))}
+                </p>
+              )}
+            </article>
+          </main>
+        </div>
+        <div className="flex flex-col space-y-4 p-4 print:hidden md:col-span-1 md:col-start-1 md:row-start-2 md:mt-6 md:items-end md:p-0">
+          <div className="w-full bg-[#EFF0F2] p-4 [&>*]:my-2">
+            <h3 className="!mt-0 text-lg font-bold">RELATED</h3>
+            {nodePage.field_stories.length && (
+              <>
+                <p className="font-bold">Stories</p>
+                <hr className="!m-0 border border-[#CDCDCD]" />
+                <p>
+                  {nodePage.field_stories.map((story) => (
+                    <a
+                      className="block text-[#03688D] hover:underline"
+                      href={story.uri}
+                    >
+                      {story.title}
+                    </a>
+                  ))}
+                </p>
+              </>
+            )}
+            {nodePage.field_products.length && (
+              <>
+                <p className="font-bold">Products</p>
+                <hr className="!m-0 border border-[#CDCDCD]" />
+                <p>
+                  {nodePage.field_products.map((product) => (
+                    <a
+                      className="block text-[#03688D] hover:underline"
+                      href={product.uri}
+                    >
+                      {product.title}
+                    </a>
+                  ))}
+                </p>
+              </>
+            )}
+            {nodePage.field_data_sets.length && (
+              <>
+                <p className="font-bold">Data Sets</p>
+                <hr className="!m-0 border border-[#CDCDCD]" />
+                <p>
+                  {nodePage.field_data_sets.map((set) => (
+                    <a
+                      className="block text-[#03688D] hover:underline"
+                      href={set.uri}
+                    >
+                      {set.title}
+                    </a>
+                  ))}
+                </p>
+              </>
+            )}
+            {nodePage.field_dvrpc_webpages.length && (
+              <>
+                <p className="font-bold">DVRPC Webpages</p>
+                <hr className="!m-0 border border-[#CDCDCD]" />
+                <p>
+                  {nodePage.field_dvrpc_webpages.map((page) => (
+                    <a
+                      className="block text-[#03688D] hover:underline"
+                      href={page.uri}
+                    >
+                      {page.title}
+                    </a>
+                  ))}
+                </p>
+              </>
+            )}
+            {nodePage.field_websites.length && (
+              <>
+                <p className="font-bold">Websites</p>
+                <hr className="!m-0 border border-[#CDCDCD]" />
+                <p>
+                  {nodePage.field_websites.map((page) => (
+                    <a
+                      className="block text-[#03688D] hover:underline"
+                      href={page.uri}
+                    >
+                      {page.title}
+                    </a>
+                  ))}
+                </p>
+              </>
+            )}
+          </div>
+          <div className="w-full bg-[#EFF0F2] p-4 [&>*]:my-2">
+            <h3 className="!mt-0 text-lg font-bold">MEDIA</h3>
+            <p className="font-bold">Resources</p>
+            <hr className="!m-0 border border-[#CDCDCD]" />
+            <p>
+              <a
+                className="text-[#03688D] hover:underline"
+                href="https://www.dvrpc.org/photosandlogos/pdf/dvrpc_logoguidelines.pdf"
+              >
+                DVRPC Logos and Guidelines
+              </a>
+              <br />
+              <a
+                className="text-[#03688D] hover:underline"
+                href="https://www.dvrpc.org/photosandlogos/"
+              >
+                Executive Director and Headshots
+              </a>
+            </p>
+            <p className="font-bold">Contact</p>
+            <hr className="!m-0 border border-[#CDCDCD]" />
+            <p className="my-2">
+              Elise Turner:{" "}
+              <a
+                className="text-[#03688D] hover:underline"
+                href="mailto:eturner@dvrpc.org"
+              >
+                eturner@dvrpc.org
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <StaffContact title={title} location={path.alias} />
+    </>
   );
 };
 
@@ -39,6 +197,35 @@ export const query = graphql`
       }
       path {
         alias
+      }
+      created
+      relationships {
+        field_image {
+          url
+        }
+        field_tags {
+          name
+        }
+      }
+      field_data_sets {
+        title
+        uri
+      }
+      field_dvrpc_webpages {
+        title
+        uri
+      }
+      field_products {
+        title
+        uri
+      }
+      field_stories {
+        title
+        uri
+      }
+      field_websites {
+        title
+        uri
       }
     }
     navItem(href: { regex: "/news/i" }) {
