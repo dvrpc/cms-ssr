378bbca5884041151e8d991ae7ec8b1885c4fa68
hachadorian <hachadorian98@gmail.com>
2023-12-08 17:06:15 -0500

    branching fix

--- src/pages/news/mediareleases.js
+++ src/pages/news/mediareleases.js
@@ -1,14 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { graphql, Link } from "gatsby";
 
+import Body from "../../components/Body";
 import HeadTemplate from "../../components/HeadTemplate";
 import StaffContact from "../../components/StaffContact";
-import Pager from "../../components/Pager";
-import { trunc } from "../../components/Product";
-import useDebounce from "../../components/useDebounce";
-import HtmlParser from "../../components/HtmlParser";
-import useQueryParamArray from "../../components/useQueryParamArray";
-import NewsRoomInfo from "../../components/NewsRoomInfo";
 
 const title = "Media Releases";
 
@@ -59,261 +54,41 @@ const themeToCustomVars = (theme, config) => {
 };
 
 const Article = ({ node }) => (
-  <li className="mb-4 list-none border-b-[3px] md:py-4">
-    <div className="text-sm text-gray-500">
-      {new Date(node.created)
-        .toLocaleDateString("en-US", {
-          month: "short",
-          day: "numeric",
-          year: "numeric",
-        })
-        .toUpperCase()}
-    </div>
+  <li className="list-group-item">
+    <div className="pb-6">
+      <div className="text-sm text-gray-500">
+        {new Date(node.created)
+          .toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+          })
+          .toUpperCase()}
+      </div>
 
-    <div className="flex flex-col-reverse md:block">
-      <p className="mb-0">
-        <Link
-          className="text-xl font-bold text-[#03688D] no-underline"
-          to={node.path.alias}
-        >
-          {node.title}
-        </Link>
-
-        {node.relationships && (
-          <p className="m-0 text-[#7A7A7A]">
-            {node.relationships.field_tags.map((tag, idx) => (
-              <>
-                <Link
-                  to={`/news/mediareleases/?filters=${tag.name.replace(
-                    " ",
-                    "-"
-                  )}`}
-                  className="no-underline hover:underline"
-                >
-                  {tag.name}
-                </Link>
-                {idx !== node.relationships.field_tags.length - 1 && ", "}
-              </>
-            ))}
-          </p>
-        )}
-      </p>
-      {node.relationships.field_image && (
-        <img
-          className="h-48 w-full border border-2 object-cover p-0.5 md:float-right md:ml-4 md:w-72"
-          src={node.relationships.field_image.url}
-          alt={node.field_image.alt}
-        />
-      )}
-    </div>
-
-    <p>
-      {node.body && node.body.summary.length ? (
-        node.body.summary
-      ) : (
-        <HtmlParser html={trunc(node.body.processed)} />
-      )}
-    </p>
-    <p>
-      <Link
-        className="flex font-bold text-[#03688D] no-underline"
-        to={node.path.alias}
-      >
-        Read More
-        <span className="my-auto mx-2 h-5 w-5 rounded-full bg-[#03688D] text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-full w-full scale-50 fill-current"
-          >
-            <path d="m5 3 3-3 12 12L8 24l-3-3 9-9z" />
-          </svg>
-        </span>
+      <Link className="text-xl no-underline" to={node.path.alias}>
+        {node.title}
       </Link>
-    </p>
+    </div>
   </li>
 );
 
-const SidebarContent = ({
-  allTaxonomyTermTags,
-  input,
-  params,
-  setParams,
-  setInput,
-}) => (
-  <div className="w-full p-4">
-    <input
-      class="w-full appearance-none border py-2 px-3 leading-tight focus:outline-none"
-      type="text"
-      placeholder="Search news stories..."
-      value={input}
-      onChange={(event) => setInput(event.target.value)}
-    ></input>
-    <span className="flex">
-      <h3 className="py-4 text-lg font-bold">FILTER RESULTS</h3>
-      <button
-        className="ml-auto text-[#03688D]"
-        onClick={() => {
-          document
-            .querySelectorAll("input[type=checkbox]")
-            .forEach((el) => (el.checked = false));
-          setParams(new Set());
-        }}
-      >
-        clear all
-      </button>
-    </span>
-    <p className="font-bold">Topic</p>
-    {allTaxonomyTermTags.edges.map((tag) => (
-      <label key={tag.node.name} className="flex items-center">
-        <input
-          className="mr-2"
-          type="checkbox"
-          value={tag.node.name}
-          onChange={(event) => {
-            setParams((prev) => {
-              if (event.target.checked) prev.add(event.target.value);
-              else prev.delete(event.target.value);
-              return new Set(prev);
-            });
-          }}
-          checked={params.size && params.has(tag.node.name)}
-        ></input>
-        {tag.node.name}
-      </label>
-    ))}
-  </div>
-);
-
 const DrupalPage = ({ data, path }) => {
-  const { allNodeArticle, userUser, allTaxonomyTermTags } = data;
-  const [articles, setArticles] = useState(allNodeArticle.edges);
-  const [input, setInput] = useState("");
-  const debounceInput = useDebounce(input);
-  const { params, setParams } = useQueryParamArray("filters");
-
-  useEffect(() => {
-    let articlesCopy = [...allNodeArticle.edges];
-    if (params.size !== 0)
-      articlesCopy = articlesCopy.filter((article) =>
-        article.node.relationships.field_tags.some((item) =>
-          params.has(item.name)
-        )
-      );
-    if (debounceInput.length)
-      articlesCopy = articlesCopy.filter((article) =>
-        article.node.title.toLowerCase().includes(debounceInput.toLowerCase())
-      );
-
-    setArticles(articlesCopy);
-  }, [params, debounceInput, allNodeArticle, setArticles]);
-
-  const toggleModal = (event) => {
-    event.preventDefault();
-    document.getElementById("modal-background").classList.toggle("hidden");
-    document.getElementById("modal").classList.toggle("hidden");
-  };
-
+  const { allNodeArticle, userUser, navItem } = data;
   return (
     <>
-      <div className="container mx-auto flex flex-col-reverse gap-x-12 print:block print:!max-w-full print:text-black sm:grid-cols-1 md:my-4 md:grid md:w-4/5 md:grid-cols-3">
-        <div className="px-4 pt-0 print:p-0 md:col-span-2 md:col-start-2 md:row-start-2 md:mt-4 md:p-0">
-          <main className="max-w-[80ch] print:max-w-full">
-            <article>
-              <p className="m-0 flex h-min">
-                {Array.from(params).map((param) => (
-                  <button
-                    className="mr-2 mb-4 flex items-center font-bold text-[#B66216]"
-                    onClick={() => {
-                      setParams((prev) => {
-                        prev.delete(param);
-                        return new Set(prev);
-                      });
-                    }}
-                  >
-                    {param}{" "}
-                    <div className="mx-1  flex h-3 w-3 items-center justify-center rounded-full bg-[#B66216] text-sm text-white">
-                      &times;
-                    </div>
-                  </button>
-                ))}
-              </p>
-              <Pager
-                items={articles}
-                onPageChange={(pageNumber) =>
-                  articles.slice(pageNumber * 5 - 5, pageNumber * 5)
-                }
-                itemsPerPage={5}
-                renderItem={(props) => (
-                  <Article key={props.node.id} {...props} />
-                )}
-              />
-            </article>
-          </main>
-        </div>
-
-        <div className="block md:hidden">
-          <p className="mb-4 bg-[#EFF0F2] p-4">
-            <input
-              class="w-full appearance-none border border-2 border-[#707070] py-2 px-3 leading-tight focus:outline-none"
-              type="text"
-              placeholder="Search news stories..."
-              value={input}
-              onChange={(event) => setInput(event.target.value)}
-            ></input>
-
-            <button
-              className="my-4 w-full rounded-lg border-2 border-[#707070] p-3 font-bold text-[#03688D] hover:bg-[]"
-              onClick={toggleModal}
-            >
-              FILTER RESULTS
-            </button>
-          </p>
-          <div
-            id="modal-background"
-            className="fixed inset-0 hidden bg-black bg-opacity-75 transition-opacity"
-          ></div>
-          <div
-            id="modal"
-            className="fixed inset-0 z-10 hidden w-screen overflow-y-auto"
-          >
-            <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
-              <div className="relative transform bg-[#EFF0F2] p-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <button
-                  className="absolute -right-3 -top-2 z-[999] mx-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#03688D] text-xl text-white"
-                  onClick={toggleModal}
-                >
-                  &times;
-                </button>
-                <SidebarContent
-                  allTaxonomyTermTags={allTaxonomyTermTags}
-                  input={input}
-                  params={params}
-                  setInput={setInput}
-                  setParams={setParams}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="hidden space-y-4 p-4 print:hidden md:col-span-1 md:col-start-1 md:row-start-2 md:mt-4 md:flex md:flex-col md:items-end md:p-0">
-          <div className="w-full bg-[#EFF0F2]">
-            <SidebarContent
-              allTaxonomyTermTags={allTaxonomyTermTags}
-              input={input}
-              params={params}
-              setInput={setInput}
-              setParams={setParams}
-            />
-          </div>
-
-          <NewsRoomInfo />
-        </div>
-      </div>
-      <div className="block w-full md:hidden">
-        <NewsRoomInfo />
-      </div>
-      <StaffContact title={title} location={path.alias} />
+      <Body title={title} menu={navItem}>
+        <ul className="list-group">
+          {allNodeArticle.edges.map((props) => (
+            <Article key={props.node.id} {...props} />
+          ))}
+        </ul>
+      </Body>
+      <StaffContact
+        staffContact={userUser}
+        title={title}
+        location={path.alias}
+      />
     </>
   );
 };
@@ -327,8 +102,17 @@ export const Head = ({ data: { nodeTheme } }) =>
   });
 
 export const query = graphql`
-  query ($limit: Int, $skip: Int) {
-    allNodeArticle(sort: { created: DESC }, limit: $limit, skip: $skip) {
+  query ($regex: String!, $limit: Int, $skip: Int) {
+    allNodeArticle(
+      sort: { created: DESC }
+      limit: $limit
+      skip: $skip
+      filter: {
+        relationships: {
+          field_tags: { elemMatch: { name: { eq: "Media Release" } } }
+        }
+      }
+    ) {
       edges {
         node {
           title
@@ -336,28 +120,6 @@ export const query = graphql`
             alias
           }
           created
-          relationships {
-            field_image {
-              url
-            }
-            field_tags {
-              name
-            }
-          }
-          field_image {
-            alt
-          }
-          body {
-            summary
-            processed
-          }
-        }
-      }
-    }
-    allTaxonomyTermTags {
-      edges {
-        node {
-          name
         }
       }
     }
@@ -366,6 +128,88 @@ export const query = graphql`
       mail
       field_display_name
       field_title
+    }
+    navItem(href: { regex: $regex }) {
+      href
+      link
+      style
+      class
+      links {
+        href
+        link
+        style
+        class
+      }
+      parent {
+        ... on NavItem {
+          href
+          link
+          style
+          class
+          links {
+            href
+            link
+            style
+            class
+          }
+          parent {
+            ... on NavItem {
+              href
+              link
+              style
+              class
+              links {
+                href
+                link
+                style
+                class
+              }
+              parent {
+                ... on NavItem {
+                  href
+                  link
+                  style
+                  class
+                  links {
+                    href
+                    link
+                    style
+                    class
+                  }
+                  parent {
+                    ... on NavItem {
+                      href
+                      link
+                      style
+                      class
+                      links {
+                        href
+                        link
+                        style
+                        class
+                      }
+                      parent {
+                        ... on NavItem {
+                          href
+                          link
+                          style
+                          class
+                          links {
+                            href
+                            link
+                            style
+                            class
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
     nodeTheme(id: { eq: "35c6397c-3d7c-5b57-9255-aedb0586ad90" }) {
       field_primary_color
--- src/pages/{NodeArticle.path__alias}.js
+++ src/pages/{NodeArticle.path__alias}.js
@@ -1,183 +1,18 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import DefaultPage from "../components/DefaultPage";
 import HeadTemplate, { themeToCustomVars } from "../components/HeadTemplate";
-import StaffContact from "../components/StaffContact";
-import HtmlParser from "../components/HtmlParser";
-import { Link } from "gatsby";
-import NewsRoomInfo from "../components/NewsRoomInfo";
 
-const BackButton = () => (
-  <Link
-    className="flex font-bold text-[#03688D] no-underline"
-    to="/news/mediareleases"
-  >
-    <span className="my-auto mr-2 h-5 w-5 rounded-full bg-[#03688D] text-white">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-full w-full scale-50 fill-current"
-      >
-        <path d="m17 0 3 3-10 9 10 9-3 3L5 12z" />
-      </svg>
-    </span>
-    Back to News Page
-  </Link>
-);
-
-const Page = ({ data: { nodePage } }) => {
-  const { body, title, path, relationships } = nodePage;
-
+const Page = ({ data: { navItem, nodePage, staffContact } }) => {
+  const { body, ...props } = nodePage;
   return (
-    <>
-      <div className="container mx-auto my-4 grid gap-x-12 print:block print:!max-w-full print:text-black sm:grid-cols-1 md:w-4/5 md:grid-cols-3">
-        <div className="px-4 pt-0 print:p-0 md:col-span-2 md:col-start-2 md:row-start-2 md:p-0">
-          <div className="flex w-full">
-            <h1 className="w-full text-3xl font-bold text-[color:var(--color-h1)] print:max-w-full print:p-0 md:col-span-1 md:col-start-2 md:max-w-[80ch] md:p-0 md:px-4 md:text-4xl">
-              {title}
-            </h1>
-          </div>
-          <main className="mt-4">
-            <article className="border-b-2">
-              <div>
-                {relationships.field_image && (
-                  <img
-                    className="h-full border border-2 object-contain p-0.5 md:float-right md:m-4 md:mt-0 md:h-64"
-                    src={relationships.field_image.url}
-                  />
-                )}
-                <p className="w-full text-sm text-[#595959]">
-                  {new Date(nodePage.created).toLocaleDateString("en-US", {
-                    month: "long",
-                    day: "numeric",
-                    year: "numeric",
-                  })}
-                </p>
-
-                {body.summary && (
-                  <blockquote className="border-l-8 border-[#91BEDC] border-opacity-[.45] pl-2 italic md:w-full">
-                    {body.summary}
-                  </blockquote>
-                )}
-                <HtmlParser html={body.processed ?? ""} />
-              </div>
-              {nodePage.relationships && (
-                <p className="m-0 text-[#7A7A7A]">
-                  {relationships.field_tags.map((tag, idx) => (
-                    <>
-                      <Link
-                        to={`/news/mediareleases/?filters=${tag.name.replace(
-                          " ",
-                          "-"
-                        )}`}
-                        className="no-underline hover:underline"
-                      >
-                        {tag.name}
-                      </Link>
-                      {idx !== relationships.field_tags.length - 1 && ", "}
-                    </>
-                  ))}
-                </p>
-              )}
-            </article>
-            <p className="hidden md:block">
-              <BackButton />
-            </p>
-          </main>
-        </div>
-        <div className="md:fle space-y-4 p-4 print:hidden md:col-span-1 md:col-start-1 md:row-start-2 md:mt-4 md:flex-col md:items-end md:p-0">
-          <BackButton />
-          <div className="w-full bg-[#EFF0F2] p-4 [&>*]:my-2">
-            <h3 className="!mt-0 text-lg font-bold">RELATED</h3>
-            {nodePage.field_stories.length > 0 && (
-              <>
-                <p className="font-bold">Stories</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
-                  {nodePage.field_stories.map((story) => (
-                    <a
-                      className="block text-[#03688D] hover:underline"
-                      href={story.uri}
-                    >
-                      {story.title}
-                    </a>
-                  ))}
-                </p>
-              </>
-            )}
-            {nodePage.field_products.length > 0 && (
-              <>
-                <p className="font-bold">Products</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
-                  {nodePage.field_products.map((product) => (
-                    <a
-                      className="block text-[#03688D] hover:underline"
-                      href={product.uri}
-                    >
-                      {product.title}
-                    </a>
-                  ))}
-                </p>
-              </>
-            )}
-            {nodePage.field_data_sets.length > 0 && (
-              <>
-                <p className="font-bold">Data Sets</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
-                  {nodePage.field_data_sets.map((set) => (
-                    <a
-                      className="block text-[#03688D] hover:underline"
-                      href={set.uri}
-                    >
-                      {set.title}
-                    </a>
-                  ))}
-                </p>
-              </>
-            )}
-            {nodePage.field_dvrpc_webpages.length > 0 && (
-              <>
-                <p className="font-bold">DVRPC Webpages</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
-                  {nodePage.field_dvrpc_webpages.map((page) => (
-                    <a
-                      className="block text-[#03688D] hover:underline"
-                      href={page.uri}
-                    >
-                      {page.title}
-                    </a>
-                  ))}
-                </p>
-              </>
-            )}
-            {nodePage.field_websites.length > 0 && (
-              <>
-                <p className="font-bold">Websites</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
-                  {nodePage.field_websites.map((page) => (
-                    <a
-                      className="block text-[#03688D] hover:underline"
-                      href={page.uri}
-                    >
-                      {page.title}
-                    </a>
-                  ))}
-                </p>
-              </>
-            )}
-          </div>
-          <div className="hidden md:block">
-            <NewsRoomInfo />
-          </div>
-        </div>
-      </div>
-      <div className="block w-full md:hidden">
-        <NewsRoomInfo />
-      </div>
-      <StaffContact title={title} location={path.alias} />
-    </>
+    <DefaultPage
+      {...props}
+      body={body.processed}
+      location={nodePage.path.alias}
+      navItem={navItem}
+      staffContact={staffContact}
+    />
   );
 };
 
@@ -204,35 +39,6 @@ export const query = graphql`
       }
       path {
         alias
-      }
-      created
-      relationships {
-        field_image {
-          url
-        }
-        field_tags {
-          name
-        }
-      }
-      field_data_sets {
-        title
-        uri
-      }
-      field_dvrpc_webpages {
-        title
-        uri
-      }
-      field_products {
-        title
-        uri
-      }
-      field_stories {
-        title
-        uri
-      }
-      field_websites {
-        title
-        uri
       }
     }
     navItem(href: { regex: "/news/i" }) {
