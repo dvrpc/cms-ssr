6e4b389bfb0bcf8925617c0dda3fd3cc36ff9034
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-12-21 10:29:21 -0500

    empty search ui

--- src/pages/news/mediareleases.js
+++ src/pages/news/mediareleases.js
@@ -284,6 +284,25 @@ const DrupalPage = ({ data, path }) => {
                   </button>
                 ))}
               </p>
+              {!articles.length && (
+                <div className="prose flex min-w-full flex-col items-center justify-center text-gray-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0,0,256,256"
+                    width="120px"
+                    height="120px"
+                  >
+                    <g fill="#d1d5db">
+                      <g transform="scale(8.53333,8.53333)">
+                        <path d="M13,3c-5.511,0 -10,4.489 -10,10c0,5.511 4.489,10 10,10c2.39651,0 4.59738,-0.85101 6.32227,-2.26367l5.9707,5.9707c0.25082,0.26124 0.62327,0.36648 0.97371,0.27512c0.35044,-0.09136 0.62411,-0.36503 0.71547,-0.71547c0.09136,-0.35044 -0.01388,-0.72289 -0.27512,-0.97371l-5.9707,-5.9707c1.41266,-1.72488 2.26367,-3.92576 2.26367,-6.32227c0,-5.511 -4.489,-10 -10,-10zM13,5c4.43012,0 8,3.56988 8,8c0,4.43012 -3.56988,8 -8,8c-4.43012,0 -8,-3.56988 -8,-8c0,-4.43012 3.56988,-8 8,-8z" />
+                      </g>
+                    </g>
+                  </svg>
+                  <h1 className="mb-0 text-2xl">Sorry no results found</h1>
+                  <div>Try searching/filtering again...</div>
+                </div>
+              )}
+
               <Pager
                 items={articles}
                 onPageChange={(pageNumber) =>
