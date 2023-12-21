e79cdd6f2344881d51744ba93dd489c4f3103a43
hachadorian <hachadorian98@gmail.com>
2023-12-20 21:19:07 -0500

    dont wrap layout for newsroom and article pages and updated headers
    remove log

--- src/pages/{NodeArticle.path__alias}.js
+++ src/pages/{NodeArticle.path__alias}.js
@@ -1,17 +1,20 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import HeadTemplate, { themeToCustomVars } from "../components/HeadTemplate";
-import StaffContact from "../components/StaffContact";
 import HtmlParser from "../components/HtmlParser";
 import { Link } from "gatsby";
-import NewsRoomInfo from "../components/NewsRoomInfo";
+import NewsRoomInfo from "../components/newsroom/NewsRoomInfo";
+import SharePage from "../components/newsroom/SharePage";
+import LogoBar from "../components/LogoBar";
+import Icon, { Search } from "../components/Icon";
+import ArticleHeader from "../images/ArticleHeader.jpg";
 
 const BackButton = () => (
   <Link
     className="flex font-bold text-[#03688D] no-underline"
     to="/news/mediareleases"
   >
-    <span className="my-auto mr-2 h-5 w-5 rounded-full bg-[#03688D] text-white">
+    <span className="my-auto mr-2 flex h-5 w-5 rounded-full bg-[#03688D] text-white">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-full w-full scale-50 fill-current"
@@ -23,28 +26,63 @@ const BackButton = () => (
   </Link>
 );
 
-const Page = ({ data: { nodePage } }) => {
+const Page = ({ data: { nodePage }, location }) => {
   const { body, title, path, relationships } = nodePage;
 
   return (
     <>
+      <header className="bg-white print:hidden">
+        <LogoBar />
+        <div
+          className="relative h-[200px] w-full bg-cover bg-center md:h-[250px]"
+          style={{ backgroundImage: `url(${ArticleHeader})` }}
+        >
+          <div className="container mx-auto p-8">
+            <form
+              className="relative mb-4 bg-gradient-to-r from-white/80 via-white/80 to-transparent md:w-min md:pr-32"
+              action="https://www.dvrpc.org/Search/"
+            >
+              <div className="pointer-events-none absolute flex h-full w-10 items-center justify-center">
+                <Icon
+                  use={Search}
+                  className="inline-block h-6 flex-shrink-0 select-none text-gray-600"
+                />
+              </div>
+              <div>
+                <input
+                  name="q"
+                  placeholder="Search..."
+                  aria-label="Search"
+                  className="m-0 block w-72 min-w-0 border-0 border-none bg-transparent p-2 pl-10 placeholder-gray-600 focus:outline-none md:pl-12"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </header>
+      <button className="absolute -mt-10 ml-7 rounded-lg border border-[#C6E6F8] p-1 px-3 text-[#C6E6F8] md:hidden">
+        Share this story
+      </button>
       <div className="container mx-auto my-4 grid gap-x-12 print:block print:!max-w-full print:text-black sm:grid-cols-1 md:w-4/5 md:grid-cols-3">
-        <div className="px-4 pt-0 print:p-0 md:col-span-2 md:col-start-2 md:row-start-2 md:p-0">
+        <div className="px-7 pt-0 print:p-0 md:col-span-2 md:col-start-2 md:row-start-2 md:p-0 md:px-7">
           <div className="flex w-full">
-            <h1 className="w-full text-3xl font-bold text-[color:var(--color-h1)] print:max-w-full print:p-0 md:col-span-1 md:col-start-2 md:max-w-[80ch] md:p-0 md:text-4xl">
+            <h1 className="w-full text-3xl font-bold text-[color:var(--color-h1)] print:max-w-full print:p-0 md:col-span-1 md:col-start-2 md:w-[80%] md:max-w-[80ch] md:p-0 md:text-4xl">
               {title}
             </h1>
+            <span className="mt-auto ml-auto hidden md:block">
+              <SharePage location={location} title={title} />
+            </span>
           </div>
           <main className="mt-4">
             <article className="border-b-2">
               <div>
                 {relationships.field_image && (
                   <img
-                    className="h-full border border-2 object-contain p-0.5 md:float-right md:m-4 md:mt-0 md:h-64"
+                    className="h-full border border-2 object-contain p-0.5 md:float-right md:m-5 md:mr-0 md:mt-0 md:h-64"
                     src={relationships.field_image.url}
                   />
                 )}
-                <p className="w-full text-sm text-[#595959]">
+                <p className="w-full text-[#595959]">
                   {new Date(nodePage.created).toLocaleDateString("en-US", {
                     month: "long",
                     day: "numeric",
@@ -53,7 +91,7 @@ const Page = ({ data: { nodePage } }) => {
                 </p>
 
                 {body.summary && (
-                  <blockquote className="border-l-8 border-[#91BEDC] border-opacity-[.45] pl-2 italic md:w-full">
+                  <blockquote className="border-[#91BEDC] border-opacity-[.45] text-lg italic leading-6 md:w-full md:border-l-8 md:pl-3">
                     {body.summary}
                   </blockquote>
                 )}
@@ -62,7 +100,7 @@ const Page = ({ data: { nodePage } }) => {
               {nodePage.relationships && (
                 <p className="m-0 text-[#7A7A7A]">
                   {relationships.field_tags.map((tag, idx) => (
-                    <>
+                    <div style={{ fontFamily: "Roboto Condensed" }}>
                       <Link
                         to={`/news/mediareleases/?filters=${tag.name.replace(
                           " ",
@@ -73,7 +111,7 @@ const Page = ({ data: { nodePage } }) => {
                         {tag.name}
                       </Link>
                       {idx !== relationships.field_tags.length - 1 && ", "}
-                    </>
+                    </div>
                   ))}
                 </p>
               )}
@@ -83,15 +121,15 @@ const Page = ({ data: { nodePage } }) => {
             </p>
           </main>
         </div>
-        <div className="md:fle space-y-4 p-4 print:hidden md:col-span-1 md:col-start-1 md:row-start-2 md:mt-4 md:flex-col md:items-end md:p-0">
+        <div className="md:fle space-y-4 p-4 px-7 print:hidden md:col-span-1 md:col-start-1 md:row-start-2 md:mt-4 md:flex-col md:items-end md:p-0 md:px-0">
           <BackButton />
-          <div className="w-full bg-[#EFF0F2] p-4 [&>*]:my-2">
-            <h3 className="!mt-0 text-lg font-bold">RELATED</h3>
+          <div className="w-full bg-[#EFF0F2] p-4">
+            <h3 className="text-lg font-bold">RELATED</h3>
             {nodePage.field_stories.length > 0 && (
               <>
-                <p className="font-bold">Stories</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
+                <p className="mt-2 mb-1 font-bold">Stories</p>
+                <hr className="!m-0 border-[#CDCDCD]" />
+                <p className="my-2">
                   {nodePage.field_stories.map((story) => (
                     <a
                       className="block text-[#03688D] hover:underline"
@@ -105,9 +143,9 @@ const Page = ({ data: { nodePage } }) => {
             )}
             {nodePage.field_products.length > 0 && (
               <>
-                <p className="font-bold">Products</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
+                <p className="mb-1 font-bold">Products</p>
+                <hr className="!m-0 border-[#CDCDCD]" />
+                <p className="my-2">
                   {nodePage.field_products.map((product) => (
                     <a
                       className="block text-[#03688D] hover:underline"
@@ -121,9 +159,9 @@ const Page = ({ data: { nodePage } }) => {
             )}
             {nodePage.field_data_sets.length > 0 && (
               <>
-                <p className="font-bold">Data Sets</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
+                <p className="mb-1 font-bold">Data Sets</p>
+                <hr className="!m-0 border-[#CDCDCD]" />
+                <p className="my-2">
                   {nodePage.field_data_sets.map((set) => (
                     <a
                       className="block text-[#03688D] hover:underline"
@@ -137,9 +175,9 @@ const Page = ({ data: { nodePage } }) => {
             )}
             {nodePage.field_dvrpc_webpages.length > 0 && (
               <>
-                <p className="font-bold">DVRPC Webpages</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
+                <p className="mb-1 font-bold">DVRPC Webpages</p>
+                <hr className="!m-0 border-[#CDCDCD]" />
+                <p className="my-2">
                   {nodePage.field_dvrpc_webpages.map((page) => (
                     <a
                       className="block text-[#03688D] hover:underline"
@@ -153,9 +191,9 @@ const Page = ({ data: { nodePage } }) => {
             )}
             {nodePage.field_websites.length > 0 && (
               <>
-                <p className="font-bold">Websites</p>
-                <hr className="!m-0 border border-[#CDCDCD]" />
-                <p>
+                <p className="mb-1 font-bold">Websites</p>
+                <hr className="!m-0 border-[#CDCDCD]" />
+                <p className="my-2">
                   {nodePage.field_websites.map((page) => (
                     <a
                       className="block text-[#03688D] hover:underline"
@@ -176,7 +214,6 @@ const Page = ({ data: { nodePage } }) => {
       <div className="block w-full md:hidden">
         <NewsRoomInfo />
       </div>
-      <StaffContact title={title} location={path.alias} />
     </>
   );
 };
