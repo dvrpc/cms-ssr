a1dd13fd5db2304134aa7ee986133fd0f1fdfb50
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2024-04-03 23:20:59 -0400

    Add parent link to navigation

--- src/components/Menu.obsolete.js
+++ src/components/Menu.obsolete.js
@@ -7,13 +7,31 @@ export default ({ data = null }) => {
   }
 
   let nodes = data.parent ? data.parent.links : [];
+  let parent = data.parent ?? data;
   if (data.links) {
     nodes = data.links;
+    parent = data;
   }
 
   return nodes.length ? (
     <nav>
       <ul className="h-full list-none sm:mb-12 sm:text-center md:text-right">
+        {parent && (
+          <li className="relative">
+            <Link
+              className="inline-block py-1 mr-5 font-bold no-underline hover:underline"
+              to={parent.href}
+              dangerouslySetInnerHTML={{ __html: parent.link }}
+            ></Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="ml-1 inline-block mb-2.5 h-3.5 absolute right-0 bottom-0"
+            >
+              <path d="M233 407c13 12 33 12 46 0l192-192a32 32 0 0 0-46-46L256 339 87 169a32 32 0 0 0-46 46l192 192z" />
+            </svg>
+          </li>
+        )}
         {nodes.map((node) => (
           <li key={node.href}>
             <Link
