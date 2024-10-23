3d4997a25f5e5212ab98a5407e2151f1efc1c96b
hachadorian <hachadorian98@gmail.com>
2024-10-23 13:59:24 -0400

    src fix

--- src/components/HtmlParser.js
+++ src/components/HtmlParser.js
@@ -93,7 +93,7 @@ const HtmlParser = ({ html }) => (
         return (
           <IframeResizer
             license="GPLv3"
-            src={src}
+            src={attribs.src}
             style={{ width: "100%", height: "100vh" }}
             waitForLoad
           />
