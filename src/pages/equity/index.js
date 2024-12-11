65170d9d986a2d8670d3a90a09ad2082264477e6
hachadorian <hachadorian98@gmail.com>
2024-12-11 16:46:15 -0500

    temp layout start

--- src/pages/equity/index.js
+++ src/pages/equity/index.js
@@ -25,12 +25,12 @@ import bgImage from "../../images/datacenter.jpg";
 import Banner from "../../components/datacenter/Banner";
 import AppCard from "../../components/datacenter/AppCard";
 import Carousel from "../../components/common/Carousel";
-import HeadTemplate from "../../components/HeadTemplate";
+import HeadTemplate, { themeToCustomVars } from "../../components/HeadTemplate";
 
 const title = "Equity";
 
 const Data = ({
-  data: { allMenuLinkContentMenuLinkContent, userUser },
+  data: { allMenuLinkContentMenuLinkContent, userUser, nodeTheme },
   location,
   serverData,
 }) => {
@@ -38,72 +38,55 @@ const Data = ({
     <>
       <header className="bg-white">
         <LogoBar />
-        <div
-          className="relative flex h-[var(--height-banner)] w-full bg-cover bg-center"
-          style={{
-            backgroundImage: `url(https://cms.dvrpc.org/sites/default/files/2021-12/header68-1600.jpg)`,
-          }}
-        >
-          <div className="m-auto w-1/2 bg-[rgba(55,65,81,0.8)] p-4 text-white">
-            <h2 className="text-[1.5rem] font-bold leading-[2rem]">
-              DVRPC Equity Hub
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
+        <div className="relative flex h-[200px] w-full bg-[#005475]">
+          <h2 className="container m-auto mx-auto p-8 text-4xl font-bold leading-[2rem] text-white">
+            DVRPC Equity Hub
+          </h2>
         </div>
       </header>
-      <div className="bg-[#def6ff] text-[#0078ae]">
-        <div className="container mx-auto grid gap-12 p-8 text-sm font-bold sm:grid-cols-2 md:grid-cols-3">
-          <div className="col-span-3">
-            <h3 className="text-2xl text-[#0078ae]">What Do You Want To Do?</h3>
-            <div className="flex flex-wrap justify-center [&>*]:w-1/3 [&>*]:bg-white">
-              <div>
-                <h3 className="text-lg">Apply For Funding Opportunities</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-              <div>
-                <h3 className="text-lg">Use Equity Tools & Data</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-              <div>
-                <h3 className="text-lg">Learn About Federal Equity Policy</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-              <div>
-                <h3 className="text-lg">
-                  See How DVRPC Is Prioritizing Equity
-                </h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-              <div>
-                <h3 className="text-lg">See How I Can Prioritize Equity</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
+      <div className="bg-[#e5e7eb] text-[#005475]">
+        <div className="container mx-auto p-8 text-sm font-bold">
+          <h3 className="mb-4 text-2xl">What Do You Want To Do?</h3>
+          <div className="grid grid-cols-3 justify-center gap-4 [&>*]:bg-white [&>*]:p-4">
+            <div>
+              <h3 className="text-lg">Apply For Funding Opportunities</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div>
+              <h3 className="text-lg">Use Equity Tools & Data</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div>
+              <h3 className="text-lg">Learn About Federal Equity Policy</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div>
+              <h3 className="text-lg">See How DVRPC Is Prioritizing Equity</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div>
+              <h3 className="text-lg">See How I Can Prioritize Equity</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="color-[#030a18] flex flex-col bg-gray-200">
+      <div className="color-[#030a18] flex flex-col">
         <div className="container mx-auto p-8">
           <h3 className="mb-0 text-2xl font-bold text-[#0078ae]">
             Featured Projects
@@ -134,19 +117,14 @@ const Data = ({
   );
 };
 
-export const Head = () =>
-  HeadTemplate({
+export const Head = ({ data }) => {
+  const { nodeTheme } = data;
+  return HeadTemplate({
     title,
-    summary:
-      "The DVRPC Data Center centralizes access to data and applications published by DVRPC for planning purposes. Watch this space for future content and enhancements as we continue to develop this site.",
-    css: `:root {
-      --color-h1: #0f1a3a;
-      --color-h2: #0f1a3a;
-      --color-h3: #0f1a3a;
-      --bg-cover-image: url(${bgImage});
-      --height-banner: 20vw;
-    }`,
+    summary: "",
+    css: themeToCustomVars(nodeTheme),
   });
+};
 
 export const query = graphql`
   query {
@@ -178,8 +156,25 @@ export const query = graphql`
         }
       }
     }
-    navItem(href: { regex: "/data/i" }) {
-      ...nestednavitem
+    nodeTheme(id: { eq: "b806b0f0-5e4c-5b9a-9d9e-efece9e24031" }) {
+      field_primary_color
+      field_secondary_color
+      field_third_color
+      field_photo_credits
+      relationships {
+        field_banner_2x {
+          uri {
+            url
+          }
+          id
+        }
+        field_banner {
+          uri {
+            url
+          }
+          id
+        }
+      }
     }
   }
 `;
