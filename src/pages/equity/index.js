643f9418e4bd7123f7f3fcfdfa10a61b92726af0
hachadorian <hachadorian98@gmail.com>
2025-01-02 12:57:27 -0500

    updated links and query

--- src/pages/equity/index.js
+++ src/pages/equity/index.js
@@ -34,7 +34,6 @@ const Data = ({
   location,
   serverData,
 }) => {
-  console.log(allNodeFeaturedEquityProject);
   return (
     <>
       <header className="bg-white">
@@ -50,35 +49,36 @@ const Data = ({
           <h3 className="mb-4 text-2xl">What Do You Want To Do?</h3>
           <div className="grid grid-cols-3 justify-center gap-4 [&>*]:bg-white [&>*]:p-4">
             <div>
-              <h3 className="text-lg">Apply For Funding Opportunities</h3>
+              <a className="text-lg" href="/equity-funding-opportunities/">
+                Apply For Funding Opportunities
+              </a>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
             <div>
-              <h3 className="text-lg">Use Equity Tools & Data</h3>
+              <a className="text-lg" href="/equity-tools-and-data/">
+                Use Equity Tools & Data
+              </a>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
             <div>
-              <h3 className="text-lg">Learn About Federal Equity Policy</h3>
+              <a className="text-lg" href="/equity-policies/">
+                Learn About Federal Equity Policy
+              </a>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
             <div>
-              <h3 className="text-lg">See How DVRPC Is Prioritizing Equity</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-            <div>
-              <h3 className="text-lg">See How I Can Prioritize Equity</h3>
+              <a className="text-lg" href="/prioritizing-equity/">
+                See Equity Projects Across the Region
+              </a>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -93,30 +93,31 @@ const Data = ({
             Featured Equity Projects
           </h3>
           <Carousel>
-            {[
-              ...allNodeFeaturedEquityProject.edges,
-              ...allNodeFeaturedEquityProject.edges,
-            ].map(({ node }) => (
+            {allNodeFeaturedEquityProject.edges.map(({ node }) => (
               <div className="mx-1 flex min-w-[90%] snap-center break-inside-avoid flex-col bg-white p-[0.25em] md:mx-2 md:min-w-[375px]">
                 <div className="relative overflow-hidden">
                   <img
-                    className="h-[150px] w-full object-cover object-center"
+                    className="h-[200px] w-full object-cover object-center"
                     src={node.relationships.field_image.url}
                   />
                 </div>
                 <div className="divide-y divide-slate-300 px-2">
                   <div className="mb-[1rem] min-h-[77px]">
                     <h4 className="m-0 w-full pt-3 text-lg text-[#0078ae]">
-                      <span className="cursor-pointer no-underline hover:underline">
+                      <a
+                        className="cursor-pointer no-underline hover:underline"
+                        href={node.field_projecturl.uri}
+                        target="_blank"
+                      >
                         {node.title}
-                      </span>
+                      </a>
                     </h4>
                   </div>
                   {node.body.processed && (
                     <div
                       className="text-base text-slate-400"
                       dangerouslySetInnerHTML={{
-                        __html: node.body.processed.slice(0, 160) + "...",
+                        __html: node.body.processed,
                       }}
                     ></div>
                   )}
@@ -173,6 +174,9 @@ export const query = graphql`
             field_image {
               url
             }
+          }
+          field_projecturl {
+            uri
           }
         }
       }
