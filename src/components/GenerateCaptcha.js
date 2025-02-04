52b85a63b069c70ceb763273e1d8a29349dc6434
hachadorian <hachadorian98@gmail.com>
2025-02-04 16:29:55 -0500

    refactor updated endpoint

--- src/components/GenerateCaptcha.js
+++ src/components/GenerateCaptcha.js
@@ -3,7 +3,6 @@ import { load } from "recaptcha-v3";
 
 const GenerateCaptcha = ({ action }) => {
   const [token, setToken] = useState("");
-  const [score, setScore] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -13,28 +12,26 @@ const GenerateCaptcha = ({ action }) => {
     })();
   }, [setToken]);
 
-  const verifyCaptcha = useCallback(
-    async (token, action) => {
-      try {
-        const req = await fetch("http://localhost:3000/", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ token, action }),
-        });
-        const res = await req.json();
-        console.log(res);
-        setScore(res.score);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    [score]
-  );
+  const verifyCaptcha = async (action) => {
+    try {
+      const req = await fetch("http://localhost:3000/recaptcha/v3", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token, action }),
+      });
+      const res = await req.json();
+      if (res.google_response.score > 0.8) return true;
+      else return false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
 
-  return { token, score, verifyCaptcha };
+  return { token, verifyCaptcha };
 };
 
 export default GenerateCaptcha;
