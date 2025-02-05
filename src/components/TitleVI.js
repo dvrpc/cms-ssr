f3d7ba4b343f8cd26a1b8436abc6910ea1367c72
hachadorian <hachadorian98@gmail.com>
2025-02-05 14:00:18 -0500

    title vi component

---
+++ src/components/TitleVI.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const TitleVI = () => {
+  return (
+    <>
+      <h2>Title VI Statement</h2>
+      <p>In effect as of January 30, 2025</p>
+      <p>
+        The Delaware Valley Regional Planning Commission (DVRPC) fully complies
+        with Title VI of the Civil Rights Act of 1964, the Civil Rights
+        Restoration Act of 1987, and related nondiscrimination mandates in all
+        programs and activities. DVRPC is committed to ensuring that no person
+        is excluded from participation in, or denied the benefits of, all
+        programs and activities on the basis of race, creed, color, national
+        origin, age, gender, disability, sexual orientation, or income level, as
+        protected by Title VI of the Civil Rights Act of 1964 and other related
+        nondiscrimination mandates.
+      </p>
+      <p>
+        DVRPC's website,{" "}
+        <a href="/" target="_blank">
+          www.dvrpc.org
+        </a>
+        , may be translated into multiple languages. Publications and other
+        public documents can be made available in alternative languages and
+        formats, if requested. DVRPC’s public meetings are always held in
+        ADA-accessible facilities and in transit-accessible locations whenever
+        possible. DVRPC will work to accommodate all reasonable requests for
+        translation, interpretation, accommodations or other auxiliary services
+        and encourages that requests be made at least seven days prior to a
+        public meeting. Requests can be made by contacting the Commission’s ADA
+        and Title VI Compliance Officer Shoshana Akins via email at
+        public_affairs@dvrpc.org, calling (215) 592-1800, or while registering
+        for an upcoming meeting.
+      </p>
+      <p>
+        Any person who believes they have been aggrieved by an unlawful
+        discriminatory practice by DVRPC under Title VI has a right to file a
+        formal complaint. Any such complaint must be in writing and filed with
+        DVRPC's ADA and Title VI Compliance Officer Shoshana Akins and/or the
+        appropriate state or federal agency within 180 days of the alleged
+        discriminatory occurrence. Complaints that a program, service, or
+        activity of DVRPC is not accessible to persons with disabilities should
+        be directed to Shoshana Akins as well. For more information on DVRPC's
+        Title VI program or to obtain a{" "}
+        <a
+          href="https://www.dvrpc.org/GetInvolved/TitleVI/pdf/TitleVIComplaintForm_English.pdf"
+          target="_blank"
+        >
+          Title VI Complaint Form
+        </a>
+        , please visit:{" "}
+        <a href="/getinvolved/titlevi" target="_blank">
+          www.dvrpc.org/GetInvolved/TitleVI
+        </a>
+        , call (215) 592-1800, or email{" "}
+        <a href="mailto:public_affairs@dvrpc.org">public_affairs@dvrpc.org</a>.
+      </p>
+    </>
+  );
+};
+
+export default TitleVI;
