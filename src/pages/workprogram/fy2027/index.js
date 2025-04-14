146295e09c907d793c38897d80e9fc809d947971
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2025-04-14 14:59:08 -0400

    Add FY27 Work Program
    for testing

---
+++ src/pages/workprogram/fy2027/index.js
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import { Link, graphql } from "gatsby";
+import Body from "../../../components/Body";
+import StaffContact from "../../../components/StaffContact";
+import HeadTemplate, {
+  defaultThemeConfig,
+  themeToCustomVars,
+} from "../../../components/HeadTemplate";
+
+const title = "FY2027 Work Program";
+
+const WorkProgramPage = ({
+  data: { userUser, navItem },
+  serverData: { projects, amendments },
+  location,
+}) => {
+  const [activeFilter, setActiveFilter] = useState(
+    "DVRPC Program Area Descriptions"
+  );
+
+  const filteredData = [...projects]
+    .filter((project) => project.subsectiontitle === activeFilter)
+    .sort((a, b) =>
+      a.proid.localeCompare(b.proid, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
+    );
+
+  console.log(amendments);
+
+  return (
+    <>
+      <Body title={title} menu={navItem}>
+        <div className="my-4">
+          At its January 25, 2024 meeting, the DVRPC Board adopted the Fiscal
+          Year (FY) 2025 Unified Planning Work Program (UPWP). This document
+          outlines all of the federally-funded planning projects slated for the
+          nine-county region from July 1, 2024 to June 30, 2025. The PDF links
+          below reflect the final version of DVRPC's adopted FY2025 UPWP as of
+          July 1, 2024. As future amendments to the FY2025 UPWP are approved,
+          they will be shown in the Work Program Amendments list.
+        </div>
+        <select
+          className="form-control"
+          onChange={(event) => setActiveFilter(event.currentTarget.value)}
+          value={activeFilter}
+        >
+          {[
+            "DVRPC Program Area Descriptions",
+            "DVRPC Project Descriptions",
+            "PA Supportive Regional Highway Planning Program (SRHPP)",
+            "NJ Supportive Regional Highway Planning Program (SRHPP)",
+            "PA Transit Support Program (TSP)",
+            "NJ Transit Support Program (TSP)",
+            "Other Member Government Projects",
+            "Work Program Amendments",
+          ].map((section) => (
+            <option key={section}>{section}</option>
+          ))}
+        </select>
+        {activeFilter === "Work Program Amendments" ? (
+          <ul className="list-group">
+            {amendments.map((amendment) => (
+              <li key={amendment.file_url} className="list-group-item">
+                <a target="_blank" href={amendment.file_url}>
+                  {amendment.projectname}
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+        <ul className="list-group">
+          {filteredData.map((project) => (
+            <li key={project.proid} className="list-group-item">
+              <Link to={`/workprogram/fy2027/${project.proid}`}>
+                {project.proid}: {project.proname}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Body>
+      <StaffContact staffContact={userUser} location={location} title={title} />
+    </>
+  );
+};
+
+export const Head = ({ data: { nodeTheme } }) =>
+  HeadTemplate({
+    title,
+    summary:
+      "The Fiscal Year (FY) 2027 Unified Planning Work Program (UPWP) outlines all of the federally-funded planning projects slated for the nine-county region.",
+    css: themeToCustomVars(nodeTheme, defaultThemeConfig),
+  });
+
+export const query = graphql`
+  query {
+    userUser(mail: { eq: "gkrykewycz@dvrpc.org" }) {
+      id
+      mail
+      name: field_display_name
+      title: field_title
+    }
+    nodeTheme(id: { eq: "0efb8b9d-ee32-58c6-897d-0a50ae2b5ac4" }) {
+      field_primary_color
+      field_secondary_color
+      field_third_color
+      field_photo_credits
+      relationships {
+        field_banner_2x {
+          uri {
+            url
+          }
+        }
+        field_banner {
+          uri {
+            url
+          }
+        }
+      }
+    }
+    navItem(href: { regex: "/^/workprogram/fy2027/?$/i" }) {
+      ...nestednavitem
+    }
+  }
+`;
+
+export default WorkProgramPage;
+
+export async function getServerData() {
+  try {
+    const res = await fetch(
+      "https://apps.dvrpc.org/ords/WORKPROGRAM27/workprogram/liveProjectsForWeb"
+    );
+    const res2 = await fetch(
+      "https://apps.dvrpc.org/ords/WORKPROGRAM27/workprogram/externalProjects"
+    );
+    if (!res.ok || !res2.ok) {
+      throw new Error("Response failed");
+    }
+    const json = await res.json();
+    const json2 = await res2.json();
+    return {
+      props: { projects: json.items, amendments: json2.items },
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      headers: {},
+      props: {},
+    };
+  }
+}
